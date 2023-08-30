3ef5ea0c08b40e2a8d84383089cda381419aaf48
superposition82 <chotnt741@gmail.com>
2023-08-31 08:38:11 +0900

    extract services

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const playwright = require('playwright'),
   chalk = require('chalk'),
   prompt = require('prompt')
 
-const { clearTerminal } = require('./helpers/cli')
 const { LOGO } = require('./constants')
 const { saveJSONToCSV } = require('./helpers')
+const { clearTerminal } = require('./helpers/cli')
+const { auth, getCompanyDetail, getCompanyList } = require('./lib/services')
 
 prompt.message = '' // 앞에 오는 메시지 제거
 
@@ -37,7 +38,7 @@ const bootstrap = async () => {
     })
 
     // 로그인
-    // TODO: 실패 시 처리
+    // TODO: 로그인 실패 처리
     console.log(chalk.green('로그인 정보를 입력하세요 ...\n'))
 
     prompt.start()
@@ -119,111 +120,10 @@ const bootstrap = async () => {
   }
 }
 
-/**
- * Logs in using the provided credentials.
- *
- * @param {playwright.BrowserContext} context
- * @param {Credentials} credentials
- * @returns {Promise<void>} A Promise that resolves when the authentication is complete.
- */
-const auth = async (context, credentials) => {
-  let page = null
-
-  try {
-    const page = await context.newPage()
-
-    await page.goto('https://www.rocketpunch.com/login')
-
-    await page.fill('#id-login-email', credentials.emailOrPhone)
-    await page.fill('#id-login-password', credentials.password)
-    await page.click("button[type='submit']")
-
-    await page.waitForNavigation()
-  } catch (error) {
-    console.error(chalk.red(`로그인에 실패하였습니다: ${error?.message}`))
-    throw error
-  } finally {
-    await page?.close()
-  }
-}
-
-/**
- * Retrieves a list of companies from a specific page number.
- *
- * @param {playwright.BrowserContext} context
- * @param {number} [pageNumber=1] - The page number to retrieve.
- * @returns {Promise<{ data: Array<{ name: string, link: string }>, hasNext: boolean }>} A Promise containing the company data and hasNext flag.
- */
-const getCompanyList = async (context, pageNumber = 1) => {
-  let page = null
-
-  try {
-    page = await context.newPage()
-
-    await page.goto(`https://www.rocketpunch.com/companies?page=${pageNumber}`)
-    await page.waitForSelector('.company-list .company.item')
-
-    const companyElements = await page.$$('.company-list .company.item')
-
-    if (companyElements.length === 0) {
-      return { data: [], hasNext: false }
-    }
-
-    const companies = await page.$$eval('.company-list .company.item', (es) =>
-      es.map((e) => ({
-        name: e.querySelector('.header.name').textContent?.trim(),
-        link: e.querySelector('.link').href?.trim()
-      }))
+bootstrap().catch((error) => {
+  console.error(
+    chalk.red(
+      `의도치 않게 프로그램이 종료되었습니다 (개발자에게 에러를 보내주세요.): ${error?.message}`
     )
-
-    const hasNext = await page.$eval(
-      '#pagination-wrapper > div > a:nth-child(4)',
-      (e) => e !== null
-    )
-
-    return { data: companies, hasNext }
-  } catch (error) {
-    console.error(
-      chalk.red(`'${page}페이지'의 정보를 가져오는 중 오류가 발생했습니다: ${error?.message}`)
-    )
-    throw error // Rethrow the error to be caught by the caller
-  } finally {
-    await page?.close()
-  }
-}
-
-/**
- * Retrieves detailed information about a company.
- *
- * @param {playwright.BrowserContext} context
- * @param {string} link - The link to the company's page.
- * @returns {Promise<{ name: string }>} A Promise containing the company's detailed information.
- */
-const getCompanyDetail = async (context, link) => {
-  let page = null
-
-  try {
-    page = await context.newPage()
-
-    await page.goto(link)
-    await page.waitForSelector('div.company-main')
-
-    const detail = await page.$eval('div.pusher', (e) => ({
-      name: e.querySelector('#company-name > h1')?.textContent?.trim(),
-      email: e.querySelector('#company-email')?.textContent?.trim() ?? '',
-      phone: e.querySelector('#company-phone')?.textContent?.trim() ?? '',
-      description: e.querySelector('#company-description')?.textContent?.trim() ?? ''
-    }))
-
-    return detail
-  } catch (error) {
-    console.error(
-      chalk.red(`'${link}'의 정보를 가져오는 중 오류가 발생했습니다: ${error?.message}`)
-    )
-    throw error
-  } finally {
-    await page?.close()
-  }
-}
-
-bootstrap().catch((error) => console.error(chalk.red(`error: ${error?.message}`)))
+  )
+})
